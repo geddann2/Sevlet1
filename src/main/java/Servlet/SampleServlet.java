440b62386fcdf87fae2dde01395652788a9cb4d9
geddann2 <geddann2@gmail.com>
2025-04-21 16:10:19 +0900

    SetReroadable

--- src/main/java/Servlet/SampleServlet.java
+++ src/main/java/Servlet/SampleServlet.java
@@ -1,11 +1,12 @@
 package Servlet;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Servlet implementation class SampleServlet
@@ -27,7 +28,6 @@ public class SampleServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
